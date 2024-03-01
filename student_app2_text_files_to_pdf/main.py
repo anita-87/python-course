9d03dcd460cf68d6f9f71d888f67f97e5f149184
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2024-03-01 18:59:54 +0100

    student_app2 final code

--- student_app2_text_files_to_pdf/main.py
+++ student_app2_text_files_to_pdf/main.py
@@ -4,11 +4,17 @@ from pathlib import Path
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 filepaths = glob.glob("PDFs/*.txt")
+
 for filepath in filepaths:
     filename = Path(filepath).stem
     title = filename.title()
     pdf.add_page()
     pdf.set_font(family="Arial", size=16, style="B")
     pdf.cell(w=0, h=16, txt=f"{title}", align="L", ln=1)
+
+    with open(filepath, 'r') as file:
+        content = file.read()
+        pdf.set_font(family="Arial", size=12)
+        pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output("output.pdf")
