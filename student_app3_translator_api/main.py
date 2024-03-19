2e336be2c8c8ba0a45cd4a15361b9a96406448c1
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2024-03-19 22:32:04 +0100

    student_app3 second exercise

--- student_app3_translator_api/main.py
+++ student_app3_translator_api/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_csv("dictionary.csv")
 
 
 @app.route("/")
@@ -10,8 +13,9 @@ def home():
 
 @app.route("/api/v1/<word>/")
 def api(word):
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
     return {
-        "definition": word.upper(),
+        "definition": definition,
         "word": word
     }
 
