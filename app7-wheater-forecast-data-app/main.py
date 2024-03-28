d41d7ae810ade45c490540591f5e8d79be99844c
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2024-03-28 22:34:58 +0100

    Day 33: Fix a couple of issues in the app

--- app7-wheater-forecast-data-app/main.py
+++ app7-wheater-forecast-data-app/main.py
@@ -13,20 +13,23 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-    # Get the temperature/sky data
-    filtered_data = get_data(place, days)
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        # Create a temperature plot
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (ºC)"})
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            # Create a temperature plot
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (ºC)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        images_paths = [images[condition] for condition in sky_conditions]
-        st.image(images_paths, width=115)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images_paths = [images[condition] for condition in sky_conditions]
+            st.image(images_paths, width=115)
 
+    except KeyError:
+        st.error(f"{place} not found!")
