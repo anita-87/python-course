3bba8abc69dceeaf5aebb9edba95affd99bd56d3
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2024-05-03 20:33:51 +0200

    Day 37 - Implement the send_email function

--- app8-email-webcam-detection/emailing.py
+++ app8-email-webcam-detection/emailing.py
@@ -1,2 +1,28 @@
-def send_email():
-    print("Email was sent!")
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    username = os.getenv("PY_EMAIL")
+    password = os.getenv("PY_PASSWORD")
+
+    gmail = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(username, password)
+    gmail.sendmail(from_addr=username, to_addrs=username, msg=email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
