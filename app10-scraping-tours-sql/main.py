7c161847b987bddec08ae5dc22d3ffbe58789ecc
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2024-05-18 20:03:16 +0200

    Day 38 - Storing extracted data in text files

--- app10-scraping-tours-sql/main.py
+++ app10-scraping-tours-sql/main.py
@@ -11,13 +11,32 @@ def scrape(url):
     return source
 
 
+def send_email():
+    print("Email was sent!")
+
+
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yml")
     value = extractor.extract(source)["tours"]
     return value
 
 
+def store(extracted_text):
+    with open("data.txt", "a") as file:
+        file.write(extracted_text + "\n")
+
+
+def read():
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scrapped = scrape(URL)
     extracted = extract(scrapped)
     print(extracted)
+    content = read()
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
